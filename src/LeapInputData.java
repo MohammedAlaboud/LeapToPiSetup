58fde3f3dd1ecac798a8ba6dd18079c633c0deef
Mohammed Alaboud <mohammed.alaboud97@gmail.com>
2019-02-17 17:18:50 +0000

    Leap to Pi Setup (with jars)
    Need to reconfigure jars when downloading project (or download jars  directly)

---
+++ src/LeapInputData.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.lang.Math;
+import com.leapmotion.leap.*;
+import com.pubnub.api.*;
+import org.json.*;
+
+public  class LeapInputData implements Runnable{
+    public static void main(String args[]) {
+
+    }
+
+    public void run() {
+        
+    }
+}
